5bf5b597673986055a54c78fb4056c9da83d3745
Omri Shtamberger <omrishtam@gmail.com>
2019-04-15 10:39:45 +0300

    switch download implementation using GetObject instead of downloader of s3manager
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- download.go
+++ download.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"io/ioutil"
 	pb "download-service/proto"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 const (
-	chunkSize int64 = 5 << 20 // 5MB per part
+	partSize int64 = 5 << 20 // 5MB per part
 )
 
 // DownloadService is a structure used for downloading files from S3
@@ -22,11 +22,6 @@ type DownloadService struct {
 // It recieves a req for a file.
 // Responds with a stream of the file bytes in chunks.
 func (s DownloadService) Download(req *pb.DownloadRequest, stream pb.Download_DownloadServer) error {
-	// Initialize downloader from client.
-	downloader := s3manager.NewDownloaderWithClient(s.s3Client, func(d *s3manager.Downloader) {
-		d.PartSize = chunkSize
-	})
-
 	// fetch key and bucket from the request and check it's validity.
 	key := req.GetKey()
 	bucket := req.GetBucket()
@@ -50,25 +45,37 @@ func (s DownloadService) Download(req *pb.DownloadRequest, stream pb.Download_Do
 	}
 
 	// Calculate how many parts there are to download.
-	totalParts := *fileDetails.ContentLength / downloader.PartSize
-	if *fileDetails.ContentLength%downloader.PartSize > 0 {
+	totalParts := *fileDetails.ContentLength / partSize
+	if *fileDetails.ContentLength % partSize > 0 {
 		totalParts++
 	}
 
 	// Iterate over all of the parts, download each part and stream it to the client.
-	for currentPart := int64(1); currentPart <= totalParts; currentPart++ {
-		buf := aws.NewWriteAtBuffer(make([]byte, 0, int(downloader.PartSize)))
-		_, err := downloader.DownloadWithContext(
-			stream.Context(),
-			buf,
-			&s3.GetObjectInput{Key: aws.String(key), Bucket: aws.String(bucket), PartNumber: aws.Int64(currentPart)},
-		)
+	for currentPart := int64(0); currentPart < totalParts; currentPart++ {
+		rangeStart := currentPart * partSize
+		rangeEnd := rangeStart + partSize - 1
+		if rangeEnd > *fileDetails.ContentLength {
+			rangeEnd = *fileDetails.ContentLength - 1
+		}
 
+		getObjectInput := &s3.GetObjectInput{
+			Key: aws.String(key),
+			Bucket: aws.String(bucket),
+			PartNumber: aws.Int64(currentPart),
+			Range: aws.String(fmt.Sprintf("bytes=%d-%d", rangeStart, rangeEnd)),
+		}
+
+		objectPartOutput, err := s.s3Client.GetObjectWithContext(stream.Context(), getObjectInput)
 		if err != nil {
 			return fmt.Errorf("failed to download file from %s/%s: %v", bucket, key, err)
 		}
 
-		stream.Send(&pb.DownloadResponse{File: buf.Bytes()})
+		partBytes, err := ioutil.ReadAll(objectPartOutput.Body)
+		if err != nil {
+			return fmt.Errorf("failed to download part %d: %v", currentPart, err)
+		}
+
+		stream.Send(&pb.DownloadResponse{File: partBytes})
 	}
 
 	return nil
