10061c22db4d9afb13e452d0c5325ca1fcd3ea5a
Omri Shtamberger <omrishtam@gmail.com>
2019-05-26 14:09:45 +0300

    update elasticsearch-logger, remove usage of logger in grpclog
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- main.go
+++ main.go
@@ -54,7 +54,6 @@ func main() {
 
 	// Make sure that log statements internal to gRPC library are logged using the logrus Logger as well.
 	logrusEntry := logrus.NewEntry(logger)
-	grpc_logrus.ReplaceGrpcLogger(logrusEntry)
 
 	// Shared options for the logger, with a custom gRPC code to log level function.
 	loggerOpts := []grpc_logrus.Option{
@@ -64,8 +63,7 @@ func main() {
 	serverOpts := append(
 		ilogger.ElasticsearchLoggerServerInterceptor(
 			logrusEntry,
-			ilogger.IgnoreMethodServerPayloadLoggingDecider("/download.Download/Download"),
-			ilogger.DefaultExtractInitialRequestDecider,
+			ilogger.IgnoreServerMethodsDecider("/download.Download/Download", os.Getenv("ELASTIC_APM_IGNORE_URLS")),
 			loggerOpts...,
 		),
 		grpc.MaxRecvMsgSize(10<<20),
